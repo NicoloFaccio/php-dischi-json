72dd06db9ba669ce528ad4b2e941db267bdc47d8
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-03-02 21:34:06 +0100

    created the header and working on the main

--- index.php
+++ index.php
@@ -7,10 +7,14 @@
 
     <!-- BOOTSTRAP'S LINK -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- PERSONAL CSS -->
+    <link rel="stylesheet" href="./src/styles/main.css">
 </head>
 <body>
     <div id="app">
-
+        <?php
+          include_once __DIR__ . '/src/components/header.php';
+        ?>
     </div>
 
     <!-- VUE'S LINK -->
---
+++ src/components/header.php
@@ -0,0 +1,7 @@
+<header>
+    <div>
+        <figure class="p-3">
+            <img class="img-fluid" width="50" height="50" src="https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg" alt="Spotify Logo">
+        </figure>
+    </div>
+</header>
